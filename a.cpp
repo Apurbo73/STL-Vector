2fbd2c9e5b8a4713cfadaa337ce80b95047f58aa
Apurbo73 <apurbodebnath895@gmail.com>
2025-03-30 17:37:47 +0600

    done

--- a.cpp
+++ a.cpp
@@ -1,9 +1,38 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
+int main() {
 
-    int a;
-    cin >> a;
+    int n;
+    cin >> n;
+    while (n--) {
+        vector < int > v;
+        int b;
+        cin >> b;
+        for (int i = 1; i <= b; i++) {
+            int c;
+            cin >> c;
+            v.push_back(i);
+        }
+
+        if (v.size() % 2 != 0 && v.size() % 3 == 0) {
+            cout << v[b / 2 + 1] << endl;
+
+
+        }
+        else if (v.size() % 2 == 0 && v.size() % 3 == 0)
+        {
+            int f = v.size();
+            cout << v[0] << " " << v[f - 1] << endl;
+        }
+        else
+        {
+            int f = v.size();
+
+            cout << v[0] + v[f - 1] << endl;
+        }
+
+    }
+
 }
